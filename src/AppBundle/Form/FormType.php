3961ec909c7fe4e371049d0db07142b532fbec5b
grzesiak8 <grzesiak8@gmail.com>
2019-05-12 13:29:52 +0200

    możliwość dodania wiele inputow we formularzu

--- src/AppBundle/Form/FormType.php
+++ src/AppBundle/Form/FormType.php
@@ -5,14 +5,17 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class FormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('input', TextType::class)
-            ->add('save', SubmitType::class)
-        ;
+            ->add('input', CollectionType::class, [
+                'entry_options' => ['label' => false],
+                'allow_add' => true ])
+            ->add('save', SubmitType::class);
+
     }
 }
