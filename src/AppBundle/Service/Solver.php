97b3a9e8440845dead6f5e3f55176a7dfe213938
grzesiak8 <grzesiak8@gmail.com>
2019-05-12 11:57:15 +0200

    dokończenie servicu do obliczeń

--- src/AppBundle/Service/Solver.php
+++ src/AppBundle/Service/Solver.php
@@ -3,8 +3,35 @@ namespace AppBundle\Service;
 
 class Solver
 {
+    private $answers = [];
     function getSolution($value)
     {
-        echo $value;
+        if($value == 0 || $value == 1) {
+            return $value;
+        }
+        $this->prepareAnswers();
+        $max = 0;
+        foreach ($this->answers as $keyAnswers => $valueAnswers) {
+            if($valueAnswers > $max) {
+                $max = $valueAnswers;
+            }
+            if($keyAnswers == $value) {
+                return $max;
+            }
+        }
+    }
+
+    private function prepareAnswers()
+    {
+        $this->answers[0] = 0;
+        $this->answers[1] = 1;
+        for($i = 2; $i <= 99999;  $i++) {
+            $tempI = floor($i / 2);
+            if($i % 2 == 0) {
+                $this->answers[$i] = $this->answers[$tempI];
+            } else {
+                $this->answers[$i] = $this->answers[$tempI] + $this->answers[($tempI + 1)];
+            }
+        }
     }
 }
